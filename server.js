1e4ab5ea5c0fcc8afd029f7582b6bd4ac2bfc594
David Yan <daveddd97@hotmail.com>
2019-11-19 01:42:40 +0100

    list api endpoint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
+const axios = require('axios')
 const port = 3000;
 const path = require("path");
 
@@ -8,6 +9,17 @@ app.use(bodyParser.json());
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/public/web/index.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/public/static/web/index.html')))
+
+app.post('/lista/brott', async (req,res) => {
+    try {
+        const response = await axios.get('https://brottsplatskartan.se/api/events/?app=skoluppgift?location=' + req.body.location);
+        console.log(response)
+        res.send(response.data.data)
+      } catch (error) {
+        console.error(error);
+        
+    }
+})
 
 app.listen(port, () => console.log('Server listening on ' + port +' '));
