85b437d9f1b3d587254ddd0b0dd6ad55ff619375
David Yan <daveddd97@hotmail.com>
2019-11-22 20:29:48 +0100

    removed logs and try catch

--- server.js
+++ server.js
@@ -15,36 +15,25 @@ app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/public/static/web/index.html')))
 
 app.post('/handelser/brott', async (req,res) => {
-    try {
-        console.log(req.body.location)
-        const response = await axios.get(baseURL + '/events/?app=' + apiKey + '&location=' + req.body.location);
-        res.send(response.data.data)
-     } catch (error) {
-        console.error(error);
-     }
+    const response = await axios.get(baseURL + '/events/?app=' + apiKey + '&location=' + req.body.location);
+    res.send(response.data.data)
 })
 
 app.get('/handelser/brott/nearby', async (req,res, next) => {
-    try {
-        const response = await axios.get('https://ipinfo.io/json');
-        let latitude = response.data.loc.substring(0, 7);
-        let longitude = response.data.loc.substring(8, 15)
-         res.locals.latitude = latitude
-         res.locals.longitude = longitude
-     } catch (error) {
-        console.error(error);
-     }
-     next()
-}, async (req, res, next) => {
-   let latitude = res.locals.latitude
-   let longitude = res.locals.longitude
-   try {
-      const response = await axios.get(baseURL + '/eventsNearby?lat=' + latitude + '&lng=' + longitude);
-      res.send(response.data.data)
-   } catch (error) {
-      console.error(error);
-   }
+    const response = await axios.get('https://ipinfo.io/json');
+    let latitude = response.data.loc.substring(0, 7);
+    let longitude = response.data.loc.substring(8, 15);
+    res.locals.latitude = latitude;
+    res.locals.longitude = longitude;
+
+    next()
+}, async (req, res) => {
+    let latitude = res.locals.latitude;
+    let longitude = res.locals.longitude;
+
+    const response = await axios.get(baseURL + '/eventsNearby?lat=' + latitude + '&lng=' + longitude);
+    res.send(response.data.data);
 }),
 
 
-app.listen(port, () => console.log('Server listening on ' + port +' '));
+app.listen(port, () => console.log('Server listening on http://localhost:' + port));
